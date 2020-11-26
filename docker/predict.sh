19daf833a08ac439d783f73cdb767a54e5d8745f
Mamun Hasan <mamunhasan@w3engineers.com>
2020-11-26 19:06:39 +0600

    # Improvement - Dockerfile update

--- docker/predict.sh
+++ docker/predict.sh
@@ -3,4 +3,4 @@
 docker ps -a | egrep 'dnn-ecg' | awk '{print $1}' | xargs docker kill
 docker ps -a | egrep 'dnn-ecg' | awk '{print $1}' | xargs docker rm
 
-docker run -d --name dnn-ecg -p 8001:8000 -v $(pwd):/src mmnhsn/tf-ecg predict.py
+docker run -i --name dnn-ecg -v $(pwd):/src mmnhsn/tf-ecg predict.py
