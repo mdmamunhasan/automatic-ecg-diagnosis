470ceb2ce15299304d25786cf7b8af4800f7949c
Mamun Hasan <mamunhasan@w3engineers.com>
2020-11-27 11:21:39 +0600

    # Improvement - pip libs update

---
+++ docker/start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+docker ps -a | egrep 'dnn-ecg' | awk '{print $1}' | xargs docker kill
+docker ps -a | egrep 'dnn-ecg' | awk '{print $1}' | xargs docker rm
+
+docker run -i --name dnn-ecg -v $(pwd):/src mmnhsn/tf-ecg $1
--- docker/train.sh
+++ docker/train.sh
@@ -3,4 +3,4 @@
 docker ps -a | egrep 'dnn-ecg' | awk '{print $1}' | xargs docker kill
 docker ps -a | egrep 'dnn-ecg' | awk '{print $1}' | xargs docker rm
 
-docker run -i --name dnn-ecg -v $(pwd):/src mmnhsn/tf-ecg predict.py
+docker run -i --name dnn-ecg -v $(pwd):/src mmnhsn/tf-ecg train.py download/data/ecg_tracings.hdf5 data/csv_files
